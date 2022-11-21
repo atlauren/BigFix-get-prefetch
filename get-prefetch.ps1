619f3afc8d659df7d2fee180e2ef9ef4bdddd177
Andrew Laurence <atlauren@uci.edu>
2022-11-21 11:57:44 -0800

    Create get-prefetch.ps1

---
+++ get-prefetch.ps1
@@ -0,0 +1,67 @@
+ <#
+
+    .SYNOPSIS
+    This PowerShell script is "get-prefetch.ps1". It provides filename, size, and sha1.
+
+    .DESCRIPTION
+    The script accepts a \path\to\file as input parameter. Wild cards are permitted. The script returns the file(s) filename, size, and sha1 hash. These are useful when building BigFix prefetch blocks.
+
+    .PARAMETER Path
+    Enter a \path\to\file on the command line.
+
+    .EXAMPLE 
+    
+    PS> get-prefetch.ps1 \path\to\file
+    *** \path\to\file ***
+      TheName: file
+      TheSize: 123
+      TheSha : [sha1 hash]
+
+    .EXAMPLE
+
+    PS> get-prefetch.ps1 \path\to\files
+    *** \path\to\file1 ***
+      TheName: file1
+      TheSize: 123
+      TheSha : [sha1 hash1]
+    *** \path\to\file2 ***
+      TheName: file
+      TheSize: 456
+      TheSha : [sha1 hash2]
+
+
+
+    .NOTES
+    atlauren@uci.edu
+    2022-11-21 First publish
+
+#>
+
+Param (
+
+	[parameter(Mandatory=$true,
+		ParameterSetName="FileSet",
+		Position=0,
+		HelpMessage="Enter the \path\to\file.")]
+	[ValidateScript(
+		{ Test-Path $_ -PathType 'Leaf' -IsValid }
+        )]
+	[string[]]$Path
+
+)
+
+$theFiles = (Get-ChildItem $Path)
+
+foreach ($file in $theFiles) {
+
+$theName = $file.Name
+$theSize = $file.Length
+$theHash = (Get-FileHash -Algorithm SHA1 -Path $file)
+$theSha = $theHash.Hash
+
+echo "*** $theName ***"
+echo "  theName = $theName"
+echo "  theSize = $theSize"
+echo "  theSha  = $theSha"
+
+} 
