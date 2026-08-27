06f79645b8cf36a639756327104ddf8914352f68
atlauren <atlauren@uci.edu>
2026-08-27 14:40:55 -0700

    Update get-prefetch.sh
    JSON options (Claude)

--- get-prefetch.sh
+++ get-prefetch.sh
@@ -7,45 +7,73 @@
 # atlauren@uci.edu
 # 2011-11-21 First publish
 # 2025-04-28 add parameter-formatted output
+# 2026-08-27 separate output sections by positional parameter
 #
 # https://github.com/atlauren/bigfix/get-prefetch.sh
 # 
 
 doHelp () {
-	echo "get-help.sh /path/to/file*"
+    echo "usage: get-prefetch.sh [--params|--json] /path/to/file*"
+    echo "  (default output: basic info)"
+    echo "  --params  output BigFix parameter block"
+    echo "  --json    output JSON blob"
 }
+
+# parse output mode flag
+outputMode="basic"
+if [[ "$1" == "--params" ]]; then
+    outputMode="params"
+    shift
+elif [[ "$1" == "--json" ]]; then
+    outputMode="json"
+    shift
+fi
 
 #vars
 files="$@"
 
 if [[ -z $files ]]; then
-	doHelp
-	exit
+    doHelp
+    exit
 fi
 
 for file in $files
 do
 
-	if [[ -f $file ]]; then
+    if [[ -f $file ]]; then
 
-		theName=$( basename $(stat -f %N $file) )
-		theSize=$( stat -f %z $file )
-		theSha=$( shasum -a 1 $file | awk '{print $1}' )
+        theName=$( basename $(stat -f %N $file) )
+        theSize=$( stat -f %z $file )
+        theSha=$( shasum -a 1 $file | awk '{print $1}' )
 
-		echo "*** $theName ***"
-		echo "  theName = $theName"
-		echo "  theSize = $theSize"
-		echo "  theSha  = $theSha"
-		echo "  ** PARAMETERS **"
-		echo -e '\t'"parameter \"theFile\" = \"$theName\""
-		echo -e '\t'"parameter \"theSha1\" = \"$theSha\""
-		echo -e '\t'"parameter \"theSize\" = \"$theSize\""
+        if [[ "$outputMode" == "basic" ]]; then
+            echo "*** $theName ***"
+            echo "  theName = $theName"
+            echo "  theSize = $theSize"
+            echo "  theSha  = $theSha"
 
-	else 
-	
-		echo "File \"$file\" does not exist."	
-		doHelp
-				
-	fi
+        elif [[ "$outputMode" == "params" ]]; then
+            echo "*** $theName ***"
+            echo "  ** PARAMETERS **"
+            echo -e '\t'"parameter \"theFile\" = \"$theName\""
+            echo -e '\t'"parameter \"theSha1\" = \"$theSha\""
+            echo -e '\t'"parameter \"theSize\" = \"$theSize\""
+
+        elif [[ "$outputMode" == "json" ]]; then
+            echo "*** $theName ***"
+            echo "  ** JSON **"
+            echo "  {"
+            echo "    \"filename\": \"$theName\","
+            echo "    \"size\": $theSize,"
+            echo "    \"sha1\": \"$theSha\""
+            echo "  }"
+        fi
+
+    else 
+    
+        echo "File \"$file\" does not exist."	
+        doHelp
+                
+    fi
 
 done
